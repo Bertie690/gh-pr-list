b1f2a87e548601e562630adef91f99fadda189d2
Bertie690 <taylormw163@gmail.com>
2025-10-23 21:57:30 -0400

    Fixed REUSE info

--- internal/projectpath/projectpath.go
+++ internal/projectpath/projectpath.go
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Matthew Taylor <taylormw163@gmail.com>
+// SPDX-FileContributor: Matthew Taylor (Bertie690)
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // package projectpath contains internal variables to get the current project root dir.
 //
 // It is only useful in testing and should not be used in production.
