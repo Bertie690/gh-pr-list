497d2722b4537f8811de930bcfc2a2b097e5d58f
Bertie690 <taylormw163@gmail.com>
2025-10-23 22:24:12 -0400

    fixed golangci-lint

--- filter/main_test.go
+++ filter/main_test.go
@@ -13,13 +13,13 @@ import (
 
 // Cleanup temp JSON files for build processing.
 func TestMain(m *testing.M) {
-	m.Run()
+	_ = m.Run()
 
 	// Don't move files on CI runs if the target files already exist
 	// (since that likely indicates gotestsum rerunning failed cases)
 	if _, err := os.Stat("../tmp/got_filter.jsonl"); os.Getenv("CI") == "" || errors.Is(err, os.ErrNotExist) {
-		os.Rename("../tmp/got.jsonl", "../tmp/got_filter.jsonl")
-		os.Rename("../tmp/want.jsonl", "../tmp/want_filter.jsonl")
-		os.Rename("../tmp/diff.jsonl", "../tmp/diff_filter.jsonl")
+		_ = os.Rename("../tmp/got.jsonl", "../tmp/got_filter.jsonl")
+		_ = os.Rename("../tmp/want.jsonl", "../tmp/want_filter.jsonl")
+		_ = os.Rename("../tmp/diff.jsonl", "../tmp/diff_filter.jsonl")
 	}
 }
--- utils/file.go
+++ utils/file.go
@@ -27,12 +27,14 @@ func ExistsDir(path string) (isDir bool, err error) {
 
 // AppendFile appends a string or byte slice to the named file, creating it if necessary.
 // It returns any error produced.
-func AppendFile[S ~string | ~[]byte](path string, data S) error {
+func AppendFile[S ~string | ~[]byte](path string, data S) (err error) {
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
 	if err != nil {
 		return fmt.Errorf("error opening file %q: \n%w", path, err)
 	}
-	defer f.Close()
+	defer func() {
+		err = errors.Join(err, f.Close())
+	}()
 
 	if _, err := f.Write([]byte(data)); err != nil {
 		return fmt.Errorf("error appending data to file %q: \n%w", path, err)
